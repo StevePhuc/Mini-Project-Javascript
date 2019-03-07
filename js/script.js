738e7525c361fb7147b52c9bcb52690bac02e0cf
Steve <contact@phuc.fi>
2019-03-07 11:46:04 +0200

    add validation for input

--- js/script.js
+++ js/script.js
@@ -1,31 +1,59 @@
 /* eslint-disable prefer-destructuring */
-const inputValue = document.querySelectorAll('.userInput input');
+const inputSelect = document.querySelectorAll('.userInput input');
 const buttonCalculate = document.querySelector('.userInput button ');
 const output = document.querySelectorAll('.output>div');
 function BMI(weigh, height) {
   return weigh / height ** 2;
 }
 
+function showError() {
+  const textError = `<b style="color:red">Please check the input. Only number. Mass in Kg. Height in meter</b>`;
+
+  const text = document.createElement('span');
+  text.innerHTML = textError;
+  // console.log(text);
+
+  document.querySelector('.userInput').appendChild(text);
+  inputSelect.forEach(input => (input.style.borderColor = 'red'));
+
+  output[2].style.backgroundColor = 'white';
+  output[2].querySelector('p').innerHTML = textError;
+}
+
+function hideError() {
+  const hideSpan = document.querySelector('.userInput span');
+  // console.log(hideSpan);
+
+  if (hideSpan) {
+    inputSelect.forEach(input => (input.style.borderColor = 'black'));
+    hideSpan.parentNode.removeChild(hideSpan);
+  }
+}
+
 const BMI_Result = [
   {
     text: 'Underweight: BMI is less than 18.5',
     imageUrl: './BMI/img/underweight.jpg',
-    color: 'orange',
+    color: 'rgba(255, 165, 1)',
+    colorBackground: 'rgba(255, 165, 1,0.5)',
   },
   {
     text: 'Normal weight: BMI is 18.5 to 24.9',
     imageUrl: './BMI/img/normal.jpg',
-    color: 'green',
+    color: 'rgba(0, 128, 0)',
+    colorBackground: 'rgba(0, 128, 0,0.5)',
   },
   {
     text: 'Overweight: BMI is 25 to 29.9',
     imageUrl: './BMI/img/overweight.jpg',
-    color: 'brown',
+    color: 'rgba(165, 42, 42,0.5)',
+    colorBackground: 'rgba(165, 42, 42,0.5)',
   },
   {
     text: 'Obese: BMI is 30 or more',
     imageUrl: './BMI/img/obese.jpg',
-    color: 'red',
+    color: 'rgba(255, 0, 0)',
+    colorBackground: 'rgba(255, 0, 0,0.5)',
   },
 ];
 
@@ -36,7 +64,7 @@ const outputText = BMI_Result.map(
 output[0].innerHTML = `<ul>${outputText}</ul>`;
 
 buttonCalculate.addEventListener('click', function() {
-  const userBMI = BMI(inputValue[0].value, inputValue[1].value);
+  const userBMI = BMI(inputSelect[0].value, inputSelect[1].value);
   // console.log(userBMI);
   // console.log(output);
   let yourBMI;
@@ -54,10 +82,18 @@ buttonCalculate.addEventListener('click', function() {
       yourBMI = BMI_Result[3];
       break;
     default:
+      yourBMI = 0;
       break;
   }
   // console.log(yourBMI);
-  output[1].querySelector('img ').setAttribute('src', yourBMI.imageUrl);
-  output[2].textContent = `Your BMI is ${userBMI.toFixed(1)}, ${yourBMI.text}`;
-  output[2].style.backgroundColor = yourBMI.color;
+  if (yourBMI != 0) {
+    hideError();
+    output[1].querySelector('img ').setAttribute('src', yourBMI.imageUrl);
+    // eslint-disable-next-line prettier/prettier
+    output[2].querySelector('p').innerHTML = `<b>Your BMI is ${userBMI.toFixed(1)}, ${yourBMI.text}</b>`;
+    output[2].style.backgroundColor = yourBMI.colorBackground;
+  } else {
+    hideError();
+    showError();
+  }
 });
